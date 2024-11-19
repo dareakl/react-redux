88db3e0b8e46fd0c11fddef0dfc42aa5dbc72271
Sathish Raja <msathis.akl@hotmail.com>
2024-11-19 18:20:54 +0800

    useSelector and useDisopatch hooks are implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-import PizzaBox from "./components/PizzaBox";
+// import PizzaBox from "./components/PizzaBox";
 import { Provider } from "react-redux";
 import store from "./components/redux/store";
+import HookContainer from "./components/HooksContainer";
 
 function App() {
   return (
     <Provider store={store}>
-      <PizzaBox />
+      {/* <PizzaBox /> */}
+      <HookContainer />
     </Provider>
   );
 }
