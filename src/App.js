b6681e960a12e1978ed0c5f5ca29526672f9f055
Sathish Raja <msathis.akl@hotmail.com>
2024-11-19 18:00:23 +0800

    connected with redux store in react app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
 import PizzaBox from "./components/PizzaBox";
+import { Provider } from "react-redux";
+import store from "./components/redux/store";
 
 function App() {
-  return <PizzaBox />;
+  return (
+    <Provider store={store}>
+      <PizzaBox />
+    </Provider>
+  );
 }
 
 export default App;
