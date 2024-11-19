bb635af38783da885d47541a14ee6a59fc0a9e4b
Sathish Raja <msathis.akl@hotmail.com>
2024-11-19 16:36:23 +0800

    initial commit react redux

---
+++ src/components/PizzaBox.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PizzaBox() {
+  return (
+    <div className="container">
+      <h2 className="text">Number of Pizza Base availabe -100</h2>
+      <button className="btn">Order Pizza</button>
+    </div>
+  );
+}
+
+export default PizzaBox;
